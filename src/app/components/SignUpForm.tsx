092b8b89b10639aa3ec52fc3b440be16f6cf55aa
Dzserold <f.dzserold@gmail.com>
2024-02-24 23:56:05 +0100

    added password hider and adjust styles in the signup page

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -1,14 +1,22 @@
-import { Input } from "@nextui-org/react";
+"use client";
+
+import { Button, Checkbox, Input, Link } from "@nextui-org/react";
 import {
   EnvelopeIcon,
+  EyeIcon,
+  EyeSlashIcon,
   KeyIcon,
   PhoneIcon,
   UserIcon,
 } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 export const SignUpForm = () => {
+  const [isVisiblePass, setIsVisiblePass] = useState(false);
+  const toggleVisible = () => setIsVisiblePass((prev) => !prev);
+
   return (
-    <form className="grid grid-cols-2 gap-3 p-2 shadow border rounded-md">
+    <form className="grid grid-cols-2 gap-3 p-2 border rounded-md shadow place-self-stretch">
       <Input label="First Name" startContent={<UserIcon className="w-4" />} />
       <Input label="Last Name" startContent={<UserIcon className="w-4" />} />
       <Input
@@ -24,15 +32,43 @@ export const SignUpForm = () => {
       <Input
         className="col-span-2"
         label="Password"
-        type="Password"
+        type={isVisiblePass ? "text" : "password"}
         startContent={<KeyIcon className="w-4" />}
+        endContent={
+          isVisiblePass ? (
+            <EyeSlashIcon
+              className="w-4 cursor-pointer"
+              onClick={toggleVisible}
+            />
+          ) : (
+            <EyeIcon className="w-4 cursor-pointer" onClick={toggleVisible} />
+          )
+        }
       />
       <Input
         className="col-span-2"
         label="Confirm Password"
-        type="Password"
+        type={isVisiblePass ? "text" : "password"}
         startContent={<KeyIcon className="w-4" />}
+        endContent={
+          isVisiblePass ? (
+            <EyeSlashIcon
+              className="w-4 cursor-pointer"
+              onClick={toggleVisible}
+            />
+          ) : (
+            <EyeIcon className="w-4 cursor-pointer" onClick={toggleVisible} />
+          )
+        }
       />
+      <Checkbox className="col-span-2">
+        I Accept The <Link href="/terms">Terms</Link>
+      </Checkbox>
+      <div className="flex justify-center col-span-2">
+        <Button className="w-48" color="primary" type="submit">
+          Submit
+        </Button>
+      </div>
     </form>
   );
 };
