f77b4ef4a5be541f60611ba0c81f93e718d2d36d
Dzserold <f.dzserold@gmail.com>
2024-02-24 23:34:43 +0100

    created signup form UI

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,12 +1,15 @@
-import { Link } from "@nextui-org/react";
+import { Link, Image } from "@nextui-org/react";
+import { SignUpForm } from "@/app/components/SignUpForm";
 
 const page = () => {
   return (
-    <div className="grid items-center justify-center grid-cols-1 md:grid-cols-2">
+    <div className="grid items-center justify-center grid-cols-1 md:grid-cols-2 gap-3">
       <div className="flex items-center justify-center md:col-span-2">
         <p className="p-2 text-center">Already Signed up?</p>
         <Link href="/auth/signin">Sign In</Link>
       </div>
+      <SignUpForm />
+      <Image src="/login.png" alt="Login Form" width={500} height={500} />
     </div>
   );
 };
--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -1,3 +1,38 @@
+import { Input } from "@nextui-org/react";
+import {
+  EnvelopeIcon,
+  KeyIcon,
+  PhoneIcon,
+  UserIcon,
+} from "@heroicons/react/20/solid";
+
 export const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  return (
+    <form className="grid grid-cols-2 gap-3 p-2 shadow border rounded-md">
+      <Input label="First Name" startContent={<UserIcon className="w-4" />} />
+      <Input label="Last Name" startContent={<UserIcon className="w-4" />} />
+      <Input
+        className="col-span-2"
+        label="Email"
+        startContent={<EnvelopeIcon className="w-4" />}
+      />
+      <Input
+        className="col-span-2"
+        label="Phone"
+        startContent={<PhoneIcon className="w-4" />}
+      />
+      <Input
+        className="col-span-2"
+        label="Password"
+        type="Password"
+        startContent={<KeyIcon className="w-4" />}
+      />
+      <Input
+        className="col-span-2"
+        label="Confirm Password"
+        type="Password"
+        startContent={<KeyIcon className="w-4" />}
+      />
+    </form>
+  );
 };
