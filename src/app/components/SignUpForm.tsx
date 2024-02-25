e154a1ec3b937a18b515e24845c75b058e695d2f
Dzserold <f.dzserold@gmail.com>
2024-02-25 23:51:43 +0100

    installed zold and made a form Schema with it in the SignUpForm.tsx

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -10,6 +10,45 @@ import {
   UserIcon,
 } from "@heroicons/react/20/solid";
 import { useState } from "react";
+import { z } from "zod";
+import validator from "validator";
+
+const FormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, "First Name must be at least 2 characters")
+      .max(45, "First Name must be less than 45 characters")
+      .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+    lastName: z
+      .string()
+      .min(2, "Last name must be at least 2 characters")
+      .max(45, "Last name must be less than 45 characters")
+      .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+    email: z.string().email("Please enter a valid email address"),
+    phone: z
+      .string()
+      // Validates the phone number with an installed package
+      .refine(validator.isMobilePhone, "Please enter a valid phone number"),
+
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(50, "Password must be at less than 50 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(50, "Password must be at less than 50 characters"),
+    accepted: z.literal(true, {
+      errorMap: () => ({
+        message: "Please accept all terms",
+      }),
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password and confirm password doesn't match",
+    path: ["password", "confirmPassword"],
+  });
 
 export const SignUpForm = () => {
   const [isVisiblePass, setIsVisiblePass] = useState(false);
