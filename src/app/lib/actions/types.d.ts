18840124ab4d2a9a4672a9f28dd1b0c4aefd6e1c
Dzserold <f.dzserold@gmail.com>
2024-03-02 22:31:22 +0100

    modified nextauth callbacks so in the Session it will give all the user info

--- src/app/lib/actions/types.d.ts
+++ src/app/lib/actions/types.d.ts
@@ -1,7 +1,15 @@
 import { User } from "@prisma/client";
 
+// In here we override the nextauth Session so it will return
+// the User schema we declared in the prisma file
 declare module "next-auth" {
   interface Session {
+    user: User;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
     user: User;
   }
 }
